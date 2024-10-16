9cde321a382416d461c78f775eebd4de29cf7dde
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2024-10-16 17:16:33 -0300

    💄 some hover changes

--- src/shared/page404.tsx
+++ src/shared/page404.tsx
@@ -4,7 +4,7 @@ import { Home } from 'lucide-react'
 
 export default function Page404() {
   return (
-    <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
+    <div className="flex min-h-[80dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
       <div className="mx-auto text-center">
         <FontAwesomeIcon icon={faTriangleExclamation} className="mx-auto h-20 w-20" />
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-foreground sm:text-8xl">404</h1>
@@ -15,7 +15,7 @@ export default function Page404() {
         <div className="mt-6">
         <a 
           href="/" 
-          className="inline-flex items-center px-6 py-3 bg-text text-primary-foreground rounded-full hover:bg-primary-600 transition-colors duration-300"
+          className="inline-flex items-center px-6 py-3 bg-text text-primary-foreground rounded-full hover:bg-blue-600 transition-colors duration-300"
         >
           <Home className="w-5 h-5 mr-2" />
           Go to Homepage
