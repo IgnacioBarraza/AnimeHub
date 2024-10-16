1a9546426b2ca1c02a79defbeb8d42e9de2546d7
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2024-10-16 10:38:17 -0300

    ✨ Add footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-background-light py-4">
+      <div className="container mx-auto px-4 text-center text-gray-400">
+        <p>&copy; 2024 Anime Hub. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
