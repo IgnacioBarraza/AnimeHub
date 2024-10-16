ba496942b391a323155408241778b9d9cb608e70
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2024-10-15 21:30:09 -0300

    ➕ ✨ Add theme changing and font awesome

--- src/shared/page404.tsx
+++ src/shared/page404.tsx
@@ -1,10 +1,12 @@
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Home } from 'lucide-react'
 
 export default function Page404() {
   return (
     <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
       <div className="mx-auto text-center">
-        {/* <FontAwesomeIcon icon={faTriangleExclamation} className="mx-auto h-12 w-12 text-primary" /> */}
+        <FontAwesomeIcon icon={faTriangleExclamation} className="mx-auto h-20 w-20" />
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-foreground sm:text-8xl">404</h1>
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-foreground sm:text-4xl">Oops, page not found!</h1>
         <p className="mt-4 text-muted-foreground">
@@ -13,7 +15,7 @@ export default function Page404() {
         <div className="mt-6">
         <a 
           href="/" 
-          className="inline-flex items-center px-6 py-3 bg-primary text-primary-foreground rounded-full hover:bg-primary-600 transition-colors duration-300"
+          className="inline-flex items-center px-6 py-3 bg-text text-primary-foreground rounded-full hover:bg-primary-600 transition-colors duration-300"
         >
           <Home className="w-5 h-5 mr-2" />
           Go to Homepage
